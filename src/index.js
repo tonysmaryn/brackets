577602b91a3c6ae7dbdbd08f0ddd18e9cb769375
Anton Maryn <tonysmaryn@gmail.com>
2020-10-15 22:39:44 +0300

    feat: copmlete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const brackets = bracketsConfig.map(element => {
+        return element.join('');
+    });
+    for (let i = 0; i < brackets.length; i++){
+        if (str.includes(brackets[i])){
+            str = str.replace(brackets[i], '');
+            i = -1;
+        }
+    }
+    return (str.length === 0);
+
 }
